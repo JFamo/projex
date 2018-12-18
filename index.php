956b128148ff14e5103548789aed65f41d51114f
J. Famous <joshua.famous@gmail.com>
2018-12-18 01:20:22 -0500

    Gut and write php for login
    This does not work rn

--- index.php
+++ index.php
@@ -15,115 +15,17 @@ function validate($data){
 
 session_start();
 
-if(isset($_POST['username']) and isset($_POST['password'])){
+if(isset($_POST['login-username']) and isset($_POST['login-password'])){
 
-	$value1 = addslashes($_POST['fullname']);
-	$value2 = addslashes($_POST['username']);
-	$value3 = addslashes($_POST['password']);
-	$value4 = addslashes($_POST['email']);
-	$value5 = $_POST['grade'];
-	$valuec = $_POST['code'];
+	$username = $_POST['login-username'];
+	$password = $_POST['login-password'];
 	
-	$value1 = validate($value1);
-	$value2 = validate($value2);
-	$value3 = validate($value3);
-	$value4 = validate($value4);
-	$value5 = validate($value5);
-	$valuec = validate($valuec);
-
-	$value3 = password_hash($value3, PASSWORD_DEFAULT);
-	
-	require_once('php/connect.php');
-	
-	$sql = "SELECT id,code,name FROM chapters";
-	
-	$result = mysqli_query($link, $sql);
-	
-	if (!$result){
-		die('Error: ' . mysqli_error($link));
-	}
-	
-	$_SESSION['chapter'] = 'nochapter';
-	$mychaptername = '';
-	
-	while(list($thisid,$thiscode,$thisname) = mysqli_fetch_array($result)){
-		if($thiscode == $valuec){
-			$_SESSION['chapter'] = $thisid;
-			$mychaptername = $thisname;
-		}
-	}
-	
-	if($_SESSION['chapter'] != 'nochapter'){
-	
-		$chapter = $_SESSION['chapter'];
-		
-		$query= "SELECT * FROM users WHERE username='$value2'";
-
-		$result = mysqli_query($link, $query);
-	
-		if (!$result){
-			die('Error: ' . mysqli_error($link));
-		}
-	
-		$count = mysqli_num_rows($result);
-	
-		if($count == 0){
-			$sqlp = "INSERT INTO users (fullname, username, password, email, grade, chapter) VALUES ('$value1', '$value2', '$value3', '$value4', '$value5', '$chapter')";
-	
-			if (!mysqli_query($link, $sqlp)){
-				die('Error: ' . mysqli_error($link));
-			}
-		
-			$mailMessage = "
-			<html>
-			<h1>Chaptersweet Account Registration</h1>
-			<p>Your account has been successfully registered with Chaptersweet.</p>
-			<p>To get started, visit <a href='http://chaptersweet.x10host.com'>http://chaptersweet.x10host.com</a>.</p>
-			<p>You are registered to chapter : </html> $mychaptername <html></p>
-			<p>Your account <b>Name</b> is : </html> $value1 <html></p>
-			<p>Your account <b>Username</b> is : </html> $value2 <html></p>
-			<p>Your account <b>Grade</b> is : </html> $value5 <html></p>
-			<p>If you have any questions or concerns, contact your advisor.</p>
-			<p>This email is automated, do not attempt to respond.</p>
-			</html>
-			";
-		
-			$headers = "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		
-			// More headers
-			$headers .= 'From: Auto-Mail <chapters@xo7.x10hosting.com>' . "\r\n";
-		
-		
-			mail($valuee,"Chaptersweet Registration",$mailMessage,$headers);
-			
-			$fmsg = 'Successfully Registered!';
-		}
-		else{
-			$fmsg = 'Username Already in Use!';
-		}
-	}
-	else{
-		$fmsg = 'Invalid Chapter Code!';
-	}
-
-}
-
-if(isset($_POST['user']) and isset($_POST['pass'])){
-
-	$sessionUsername = $_POST['user'];
-	$sessionPassword = $_POST['pass'];
-	$chapter = $_POST['chapter'];
-	
-	$sessionUsername = validate($sessionUsername);
-	$sessionPassword = validate($sessionPassword);
-	$chapter = validate($chapter);
-	
-	$_SESSION['chapter'] = $chapter;
+	$username = validate($username);
+	$password = validate($password);
 	
 	require('php/connect.php');
 	
-	$query= "SELECT id FROM users WHERE username='$sessionUsername' AND chapter='$chapter'";
+	$query= "SELECT id FROM users WHERE username='$username'";
 
 	$result = mysqli_query($link, $query);
 
@@ -136,35 +38,20 @@ if(isset($_POST['user']) and isset($_POST['pass'])){
 	if($count == 1){
 
 		//fetch the rank of that user
-		$query2 = "SELECT id, fullname, rank, grade, eventpoints, idnumber, password FROM users WHERE username='$sessionUsername' AND chapter='$chapter'";
+		$query2 = "SELECT id, password, firstname, lastname FROM users WHERE username='$username'";
 		$result2 = mysqli_query($link, $query2);
 		if (!$result2){
 			die('Error: ' . mysqli_error($link));
 		}
 
-		list($idValue, $fullnameValue, $rankValue, $gradeValue, $eventPointsValue, $idnumber, $password) = mysqli_fetch_array($result2);
+		list($idValue, $passwordValue, $firstnameValue, $lastnameValue) = mysqli_fetch_array($result2);
 
-		if(password_verify($sessionPassword, $password)){
+		if(password_verify($password, $passwordValue)){
 
 			$_SESSION['id'] = $idValue;
-			$_SESSION['username'] = $sessionUsername;
-			$_SESSION['rank'] = $rankValue;
-			$_SESSION['fullname'] = $fullnameValue;
-			$_SESSION['grade'] = $gradeValue;
-			$_SESSION['eventpoints'] = $eventPointsValue;
-			
-			//get the conference
-			$conferencequery="SELECT value FROM settings WHERE name='conference' AND chapter='$chapter'";
-			
-			$conferenceresult = mysqli_query($link, $conferencequery);
-			
-			if (!$conferenceresult){
-				die('Error: ' . mysqli_error($link));
-			}
-			
-			list($conference) = mysqli_fetch_array($conferenceresult);
-			
-			$_SESSION['conference'] = $conference;
+			$_SESSION['username'] = $username;
+			$_SESSION['firstname'] = $firstnameValue;
+			$_SESSION['lastname'] = $lastnameValue;
 
 		}
 
@@ -220,12 +107,19 @@ if(isset($_SESSION['username'])){
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-12">
+				<?php
+
+					if(isset($fmsg)){
+						echo "<p>" . $fmsg . "</p>";
+					}
+
+				?>
 				<div class="row">
 					<div class="col-12">
 						<center>
 							<h3 class="pt-4">Login</h3>
 						</center>
-						<form class="pt-4">
+						<form method="post" class="pt-4">
 						  <div class="form-row">
 						    <div class="form-group col-md-6">
 						      <label for="login-username">Username</label>
@@ -245,7 +139,7 @@ if(isset($_SESSION['username'])){
 						<center>
 							<h3 class="pt-4">Register</h3>
 						</center>
-						<form class="py-4">
+						<form method="post" class="py-4">
 						  <div class="form-row">
 						    <div class="form-group col-md-6">
 						      <label for="reigster-username">Username</label>
