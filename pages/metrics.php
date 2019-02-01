72ff78b55879e59b1e7d9bbd08c751d8b93a05f7
J. Famous <joshua.famous@gmail.com>
2019-02-01 01:45:13 -0500

    Tom I'm sorry

--- pages/metrics.php
+++ pages/metrics.php
@@ -15,6 +15,20 @@ function validate($data){
 
 session_start();
 
+//Handle Changing Workspaces
+if(isset($_POST['workspace-id'])){
+
+  $newworkspace = $_POST['workspace-id'];
+  //~~JOSH~~
+  //Need checking that the user really has this workspace here
+  //Prevents client-side editing of workspace value to access those of other orgs
+  //@Tom
+  
+  $_SESSION['workspace'] = $newworkspace;
+  $_SESSION['project'] = null;
+
+}
+
 if(!isset($_SESSION['username'])){
 
 	header('Location: ../index.php');
@@ -34,12 +48,10 @@ if(!isset($_SESSION['username'])){
     <script src="../js/popper.min.js"></script>
     <script src="../bootstrap-4.1.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
-
-    <!-- Google Fonts - Changes to come -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i" rel="stylesheet">
 	
 	<title>
-		ProjeX Metrics
+		ProjeX
 	</title>
 
 	<!-- Import our CSS -->
@@ -49,88 +61,82 @@ if(!isset($_SESSION['username'])){
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Stuff for sidebar -->
-	    <style>
-    	.sidenav {
-		  height: 100%;
-		  width: 0;
-		  position: fixed;
-		  z-index: 1;
-		  top: 0;
-		  left: 0;
-		  background-color: #111;
-		  overflow-x: hidden;
-		  transition: 0.5s;
-		  padding-top: 60px;
-		}
-
-		.sidenav a {
-		  padding: 8px 8px 8px 32px;
-		  text-decoration: none;
-		  font-size: 25px;
-		  color: #818181;
-		  display: block;
-		  transition: 0.3s;
-		}
-
-		.sidenav a:hover {
-		  color: #f1f1f1;
-		}
-
-		.sidenav .closebtn {
-		  position: absolute;
-		  top: 0;
-		  right: 25px;
-		  font-size: 36px;
-		  margin-left: 50px;
-		}
-		@media screen and (max-height: 450px) {
-		  .sidenav {padding-top: 15px;}
-		  .sidenav a {font-size: 18px;}
-		}
-		</style>
-	</head>
-
 <body>
 <!-- Navbar -->
-	<nav class="navbar navbar-dark bg-primary">
-		<div class="navpadder">
-		  	<a class="nav-link" href="#" style="flex-basis:20%;"><img src="" width="30" height="30" class="d-inline-block align-top" alt="" />ProjeX</a>
-		  	<a class="nav-link" href="#"><img src="../imgs/workspacePlaceholder.png" width="30" height="30" class="d-inline-block align-top" alt="" /></a>
-		    <a class="nav-link" href="metrics.php">Metrics</a>
-		    <a class="nav-link" href="metrics.php">Backlog</a>
-		    <a class="nav-link" href="metrics.php">Active</a>
-		    <a class="nav-link" href="metrics.php">Docs</a>
-		    <a class="nav-link" href="metrics.php">Messages</a>
-		    <a class="nav-link" href="../php/logout.php">Logout</a>
-	    </div>
+	<nav class="navbar navbar-dark bg-grey pb_bottom">
+	  	<span id="openNavButton" style="font-size:30px;cursor:pointer;color:white;padding-right:30px;" onclick="toggleNav()">&#9776;</span>
+	    <a class="nav-link" href="../php/logout.php">Logout</a>
 	</nav>
 
-<!-- Sidebar for graphs -->
-
-	<div id="mySidenav" class="sidenav">
-	  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-	  <a href="#">Velocity Chart</a>
-	  <a href="/projex/pages/relativeContribution.php">Relative Contribution</a>
-	  <a href="/projex/pages/sprintReport.php">Sprint Report</a>
-	</div>
-
-	<h2>Velocity Graph</h2>
-	<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Charts</span>
-
-	<script>
-	function openNav() {
-	  document.getElementById("mySidenav").style.width = "250px";
-	}
-
-	function closeNav() {
-	  document.getElementById("mySidenav").style.width = "0";
-	}
-	</script>
 <!--Spooky stuff in the middle-->
-
-	<div class="container-fluid bodycontainer">
+	<div class="container-fluid">
 		<div class="row">
+			<div id="mySidenav" style="padding-right:0; padding-left:0;" class="sidenav bg-grey">
+				<nav style="width:100%;" class="navbar navbar-dark">
+				  <div class="container" style="padding-left:0px;">
+				  <ul class="nav navbar-nav align-top">
+				   <a class="navbar-brand icon" href="#"><img src="../imgs/workspacePlaceholder.png" alt="icon" width="60" height="60">Projex</a>
+				   <div class="dropdown">
+              <div class="btn-group dropright">
+                <button type="button" class="btn btn-secondary"><?php 
+                  require('../php/connect.php');
+                  $workspace = $_SESSION['workspace'];
+                  $query = "SELECT name FROM workspaces WHERE id='$workspace'";
+                  $result = mysqli_query($link, $query);
+                  if (!$result){
+                    die('Error: ' . mysqli_error($link));
+                  }
+                  list($name) = mysqli_fetch_array($result);
+                  if($_SESSION['workspace'] == null || $_SESSION['workspace'] == null){
+                    echo "Select a Workspace";
+                  }
+                  else{
+                    echo $name;
+                  }
+                ?></button>
+                <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+              <div class="dropdown-menu dropdown-menu-right">
+
+                <?php
+
+                require('../php/connect.php');
+
+                $username = $_SESSION['username'];
+                $query = "SELECT workspaces.name, workspaces.id FROM ((user_workspace_mapping INNER JOIN workspaces ON workspaces.id = user_workspace_mapping.workspace) INNER JOIN users ON user_workspace_mapping.user = users.id) WHERE users.username = '$username'";
+                $result = mysqli_query($link, $query);
+                if (!$result){
+                  die('Error: ' . mysqli_error($link));
+                }
+                while($resultArray = mysqli_fetch_array($result)){
+                $workspaceName = $resultArray['name'];
+                $workspaceID = $resultArray['id'];
+
+                ?>
+                <form method="POST"><input type="hidden" value="<?php echo $workspaceID; ?>" name="workspace-id"/><input class="dropdown-item <?php if($_SESSION['workspace'] == $workspaceID){ echo 'active-dropdown'; } ?>" type="submit" value="<?php echo $workspaceName; ?>"></form>
+                <?php } ?>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="workspace.php">Create New</a>
+              </div>
+            </div>
+            </div>
+			        <hr class="sidenavHR">
+			        <a class="nav-link active" href="main.php">Dashboard</a>
+				    <a class="nav-link" href="metrics.php">Metrics</a>
+				    <a class="nav-link" href="backlog.php">Backlog</a>
+				    <a class="nav-link" href="active.php">Active</a>
+				    <a class="nav-link" href="docs.php">Docs</a>
+				    <a class="nav-link" href="messages.php">Messages</a>
+				    <hr class="sidenavHR">
+				    <a class="nav-link" href="account.php">My Account</a>
+				    <a class="nav-link" href="organization.php">My Organization</a>
+				  </ul>
+				  </div>
+				</nav>
+			</div>
+			<div id="pageBody">
+			<div class="row">
 			<div class="col-sm-8">
 			<canvas id="veloChart" class="dashchart"></canvas>
 				<script>
@@ -185,7 +191,7 @@ if(!isset($_SESSION['username'])){
 			</div>
 		</div>
 	</div>
-		<div>
+	<div>
 </body>
 
 <footer class="text-white bg-primary py-3 h5"> 
